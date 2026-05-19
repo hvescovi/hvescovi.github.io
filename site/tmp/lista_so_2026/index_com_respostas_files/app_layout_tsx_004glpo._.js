d20634827f321ecc9a4d18e2f1937522fae57cfb
Hylson <hvescovi@gmail.com>
2026-05-19 13:47:53 -0300

    docs:with answers

---
+++ site/tmp/lista_so_2026/index_com_respostas_files/app_layout_tsx_004glpo._.js
@@ -0,0 +1,5 @@
+(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: ["static/chunks/app_globals_0jn8.0u.css","static/chunks/node_modules_next_dist_0tt2wve._.js"],
+    source: "dynamic"
+});
