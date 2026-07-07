1fc131ff4240518aeca66dcb1d12906d8989fe50
Hylson <hvescovi@gmail.com>
2026-07-07 13:19:56 -0300

    removing tmp

--- tmp/lista_poo2_2026/lista_files/app_layout_tsx_004glpo._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/app_globals_0jn8.0u.css","static/chunks/node_modules_next_dist_0tt2wve._.js"],
-    source: "dynamic"
-});
